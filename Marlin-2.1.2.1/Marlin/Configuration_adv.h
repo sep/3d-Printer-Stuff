b3fdc6347f9d137398226406265afbac4d2194d0
CooperZuranskiSEP <cmzuranski@sep.com>
2024-12-13 12:24:54 -0500

    attempt fix print pausing

--- Marlin-2.1.2.1/Marlin/Configuration_adv.h
+++ Marlin-2.1.2.1/Marlin/Configuration_adv.h
@@ -2387,7 +2387,7 @@
 #if BOTH(SDSUPPORT, DIRECT_STEPPING)
   #define BLOCK_BUFFER_SIZE  8
 #elif ENABLED(SDSUPPORT)
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE 32
 #else
   #define BLOCK_BUFFER_SIZE 16
 #endif
@@ -2396,7 +2396,7 @@
 
 // The ASCII buffer for serial input
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 4
+#define BUFSIZE 32
 
 // Transmission to Host Buffer Size
 // To save 386 bytes of flash (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
